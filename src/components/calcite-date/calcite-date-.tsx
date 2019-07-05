5abde6de7d4d80ca6e6e9c10c7ae29c148a0c1da
Kumar Gayatri <KGayatri@esri.com>
2019-07-05 13:11:38 -0700

    Added Date picker as per the UX design

---
+++ src/components/calcite-date/calcite-date-.tsx
@@ -0,0 +1,339 @@
+import {
+  Component,
+  h,
+  Prop,
+  Event,
+  Element,
+  Host,
+  EventEmitter,
+  Watch,
+  State
+} from "@stencil/core";
+import { ENTER } from "../../utils/keys";
+
+@Component({
+  tag: "calcite-date",
+  styleUrl: "calcite-date.scss",
+  shadow: true
+})
+export class CalciteDate {
+  @Element() el: HTMLElement;
+  /**
+   * Value of the form control
+   */
+  @Prop() value?: string;
+  /**
+   * Name of the form control (useful for specifying input/label relationship)
+   */
+  @Prop() min?: string = "";
+  /**
+   * Value of the form control
+   */
+  @Prop() max?: string = "";
+
+  @Prop() prevMonthLabel?: string = "";
+  @Prop() nextMonthLabel?: string = "";
+  /**
+   * Sun by default
+   * 0: Sunday
+   * 1: Monday
+   * 2: Tuesday
+   * 3: Wednesday
+   * 4: Thursday
+   * 5: Friday
+   * 6: Saturday
+   */
+  @Prop() startOfWeek?: number = 0;
+
+  @Prop() locale?: string = "en-US";
+
+  /**
+   * @todo document what gets passed to the handler for these events
+   */
+  @Event() calciteDateChange: EventEmitter;
+
+  private observer: MutationObserver;
+  @State() month: number = new Date(this.value).getMonth();
+  @State() year: number = new Date(this.value).getFullYear();
+  @State() showCalendar: any = false;
+
+  @Watch("switched") switchWatcher() {
+    this.calciteDateChange.emit();
+  }
+
+  private inputProxy: HTMLInputElement;
+
+  connectedCallback() {
+    this.setupProxyInput();
+  }
+
+  disconnectedCallback() {
+    this.observer.disconnect();
+  }
+
+  componentWillRender() {
+    this.syncProxyInputToThis();
+  }
+
+  selectPrevMonth() {
+    if(this.month === 0 ) {
+      this.year = this.year - 1;
+    }
+    this.month = (12 + this.month -1) %12;
+  }
+
+  selectPrevMonthOnEnter(event: KeyboardEvent){
+    if(event.keyCode === ENTER){
+      this.selectPrevMonth();
+    }
+  }
+
+  selectNextMonth() {
+    if(this.month === 11){
+      this.year = this.year + 1;
+    }
+    this.month = (this.month + 1) % 12;
+  }
+
+  selectNextMonthOnEnter(event: KeyboardEvent) {
+    if(event.keyCode === ENTER){
+      this.selectNextMonth();
+    }
+  }
+
+  onYearChange(event) {
+    this.year = parseInt(event.target.value);
+  }
+
+  render() {
+    const selectedDate = this.value ? new Date(`${this.value} `) : new Date();
+    selectedDate.setMonth(this.month || selectedDate.getMonth());
+    selectedDate.setFullYear(this.year || selectedDate.getFullYear());
+    return (
+      <Host>
+        <div class={`${this.showCalendar ? "host-expanded" : ""}`}>
+        <div class={`date-input-wrapper ${this.showCalendar ? "expanded" : ""}`} role="application" onBlur = {() => this.closeCalendar()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="calendar-icon"
+            viewBox="0 0 16 16"
+            width="16"
+            height="16"
+          >
+            <path d="M16 16H0V6h16zM3 7H1v2h2zm3 0H4v2h2zm3 0H7v2h2zm3 0h-2v2h2zm3 0h-2v2h2zM3 10H1v2h2zm3 0H4v2h2zm3 0H7v2h2zm3 0h-2v2h2zm3 0h-2v2h2zM3 13H1v2h2zm3 0H4v2h2zm3 0H7v2h2zm3 0h-2v2h2zm3 0h-2v2h2zM5 2V1h6v1zm9-1v1h1v2H1V2h1V1H0v4h16V1zM4 0H3v2h1zm9 0h-1v2h1z" />
+          </svg>
+          <input
+            type="text"
+            placeholder="dd/mm/yyyy"
+            value={this.value}
+            class="date-input"
+            onFocus = {() => this.expandCalendar()}
+          />
+        </div>
+        { this.showCalendar ? 
+        <div class="calendar-picker-wrapper">
+          {this.renderMonths(
+            this.getMonth(selectedDate),
+            this.getYear(selectedDate)
+          )}
+          {this.renderCalendar(
+            this.getMonth(selectedDate),
+            this.getYear(selectedDate),
+            selectedDate.getDate()
+          )}
+        </div> : ""
+        }
+        <slot />
+        </div>
+      </Host>
+    );
+  }
+
+  private expandCalendar() {
+    this.showCalendar = true;
+  }
+
+  private closeCalendar() {
+    this.showCalendar = false;
+  }
+
+  private getMonth(date) {
+    return this.month === undefined ? date.getMonth() : this.month;
+  }
+
+  private getYear(date) {
+    return this.year === undefined ? date.getFullYear() : this.year;
+  }
+
+  private getLocalizedMonths() {
+    let m = 0,
+      months = [],
+      date = new Date();
+    for (; m < 12; m++) {
+      date.setMonth(m);
+      months.push(
+        new Intl.DateTimeFormat(this.locale, {
+          month: "long"
+        }).format(date)
+      );
+    }
+
+    return months;
+  }
+
+  private getLocalizedWeekday() {
+    let w = 1,
+      startWeek = [],
+      endWeek = [],
+      date = new Date();
+    for (; w < 8; w++) {
+      date.setDate(w);
+      let day = new Intl.DateTimeFormat(this.locale, {
+        weekday: "short"
+      }).format(date);
+      date.getDay() === this.startOfWeek || startWeek.length > 0
+        ? startWeek.push(day)
+        : endWeek.push(day);
+    }
+
+    return [...startWeek, ...endWeek];
+  }
+
+  private renderMonths(month, year) {
+    const localizedMonth = this.getLocalizedMonths()[month];
+
+    return (
+      <div class="month-year" aria-hidden="true">
+        <span role="button" aria-label={this.prevMonthLabel} tabindex={0} onClick ={() => this.selectPrevMonth()} onKeyDown ={(event) => this.selectPrevMonthOnEnter(event)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="left-icon"
+            viewBox="0 0 16 16"
+            height="16"
+            width="16"
+          >
+            <path d="M11.783 14H9.017l-6-6 6-6h2.766l-6 6z" />
+          </svg>
+        </span>
+        <span class="month" role="heading" aria-live="assertive" aria-atomic="true">{localizedMonth}</span>
+        <input
+          class="year"
+          type="number"
+          value={year}
+          style={{ width: `${(`${year}`.length + 1) * 11}px` }}
+          onChange={ (event) => this.onYearChange(event) }
+        />
+        <span role="button" aria-label={this.nextMonthLabel} tabindex={0} onClick ={() => this.selectNextMonth()} onKeyDown ={(event) => this.selectNextMonthOnEnter(event)} >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="right-icon"
+            viewBox="0 0 16 16"
+            height="16"
+            width="16"
+          >
+            <path d="M10.217 8l-6-6h2.766l6 6-6 6H4.217z" />
+          </svg>
+        </span>
+      </div>
+    );
+  }
+
+  private getPrevMonthdays(month, year) {
+    let startDay = new Date(year, month, 1).getDay(),
+      days = [],
+      prevMonDays = new Date(year, month, 0).getDate();
+    if (startDay === this.startOfWeek) {
+      return days;
+    }
+    for (let i = (6 - this.startOfWeek + startDay) %7; i >=0; i--) {
+      days.push(prevMonDays - i);
+    }
+    return days;
+  }
+
+  private getNextMonthdays(month, year) {
+    let endDay = new Date(year, month + 1, 0).getDay(),
+      days = [];
+    if (endDay === ((this.startOfWeek + 6) % 7)) {
+      return days;
+    }
+
+    return [...Array((6 - (endDay - this.startOfWeek)) % 7).keys()];
+  }
+
+  private renderDay(day, isSelected = false){
+    return <td class={`cur-day day ${isSelected ? "selected-day" : ""}`} onClick={() => this.setDate(day)} onKeyPress={(event) => this.keyPress(event, day)} role="gridcell" tabindex={isSelected ? 0 : -1}>{day}</td>
+  }
+
+  keyPress(event, day){
+    if(event.keyCode === ENTER){
+      this.setDate(day);
+    }
+  }
+
+  private renderCalendar(month, year, selectedDate) {
+    let weekDays = this.getLocalizedWeekday(),
+      curMonDays = [...Array(new Date(year, month + 1, 0).getDate()).keys()],
+      prevMonDays = this.getPrevMonthdays(month, year),
+      nextMonDays = this.getNextMonthdays(month, year),
+      splitDays = [],
+      days = [
+        ...prevMonDays.map(prev => this.renderDay(prev)),
+        ...curMonDays.map(cur => this.renderDay(cur+1, cur+1 === selectedDate)),
+        ...nextMonDays.map(next => this.renderDay(next+1))
+      ];
+    for (let i = 0; i < days.length; i += 7)
+      splitDays.push(days.slice(i, i + 7));
+
+      
+    return (
+      <table class="calender" role="grid">
+        <thead class="weekdays" role="presentation">
+          {weekDays.map(weekday => (
+            <th class="weekday" role="columnheader">{weekday}</th>
+          ))}
+        </thead>
+        <tbody class="calendar-days" aria-hidden={true}>
+          {splitDays.map(days => (
+            <tr class="week" role="row">{days}</tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+  
+  setDate(day) {
+    this.value = new Date(this.year, this.month, day).toISOString().substr(0,10);
+    this.syncProxyInputToThis();
+    this.calciteDateChange.emit();
+  }
+
+  private setupProxyInput() {
+    // check for a proxy input
+    this.inputProxy = this.el.querySelector("input");
+
+    // if the user didn't pass a proxy input create one for them
+    if (!this.inputProxy) {
+      this.inputProxy = document.createElement("input");
+      this.inputProxy.type = "date";
+      this.syncProxyInputToThis();
+      this.el.appendChild(this.inputProxy);
+    }
+
+    this.syncThisToProxyInput();
+    this.observer = new MutationObserver(this.syncThisToProxyInput);
+    this.observer.observe(this.inputProxy, { attributes: true });
+  }
+
+  private syncThisToProxyInput = () => {
+    this.value = this.inputProxy.value;
+    this.min = this.inputProxy.min;
+    this.max = this.inputProxy.max;
+  };
+
+  private syncProxyInputToThis = () => {
+    this.inputProxy.value = new Date(`${this.value} `).toISOString().substr(0,10);
+    this.inputProxy.min = this.min;
+    this.inputProxy.max = this.max;
+  };
+}
